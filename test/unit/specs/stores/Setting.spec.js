09ca5ddd2014343ab46600d49b3a94a44170add8
Katsuya HIDAKA <hidakatsuya@gmail.com>
2017-02-24 03:00:18 +0900

    Store specs

---
+++ test/unit/specs/stores/Setting.spec.js
@@ -0,0 +1,27 @@
+import setting from 'src/stores/Setting'
+
+describe('setting', () => {
+  it('constructor', () => {
+    expect(setting.storage).to.be.an.instanceof(Storage)
+  })
+
+  it('#authorizationKey=', () => {
+    setting.authorizationKey = 'abcd'
+    expect(window.localStorage.getItem('authorizationKey')).to.equal('abcd')
+  })
+
+  it('#nippouPath=', () => {
+    setting.nippouPath = '/path/to/nippous'
+    expect(window.localStorage.getItem('nippouPath')).to.equal('/path/to/nippous')
+  })
+
+  it('#authorizationKey', () => {
+    window.localStorage.setItem('authorizationKey', 'xxx')
+    expect(setting.authorizationKey).to.equal('xxx')
+  })
+
+  it('#nippouPath', () => {
+    window.localStorage.setItem('nippouPath', '/path')
+    expect(setting.nippouPath).to.equal('/path')
+  })
+})
