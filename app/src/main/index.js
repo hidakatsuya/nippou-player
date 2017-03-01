24387fd0df613e94d72a9f3476ee4ac992045633
Katsuya HIDAKA <hidakatsuya@gmail.com>
2017-03-01 13:06:56 +0900

    Enable copy and paste

--- app/src/main/index.js
+++ app/src/main/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, Menu } from 'electron'
 
 let mainWindow
 const winURL = process.env.NODE_ENV === 'development'
@@ -22,6 +22,29 @@ function createWindow () {
   mainWindow.on('closed', () => {
     mainWindow = null
   })
+
+  const template = [
+    {
+      label: 'Application',
+      submenu: [
+        { label: 'Quit', accelerator: 'Command+Q', click: () => { app.quit() } }
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+        { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+        { type: 'separator' },
+        { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+        { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+        { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+        { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
+      ]
+    }
+  ]
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
 
   // eslint-disable-next-line no-console
   console.log('mainWindow opened')
