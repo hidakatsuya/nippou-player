4e4732b8fde0f981d55ae654eb5360309c4aecbc
Katsuya Hidaka <hidakatsuya@gmail.com>
2019-11-03 18:21:30 +0900

    Add sectionTitles setting

--- app/src/renderer/components/Setting/index.vue
+++ app/src/renderer/components/Setting/index.vue
@@ -16,7 +16,13 @@
             日報一覧のパス
             <help-button :href="'https://docs.esa.io/posts/104#2-0-0'" />
           </label>
-          <input type="text" v-model="setting.nippouPath" placeholder="in:日報/YYYY/MM/DD" class="form-control">
+          <input type="text" v-model="setting.nippouPath" placeholder="in:日報/YYYY/MM/DD" class="form-control">
+        </div>
+        <div class="form-group">
+          <label>
+            対象セクション
+          </label>
+          <textarea v-model="setting.sectionTitles" placeholder="# セクション1" class="form-control" rows="4"></textarea>
         </div>
       </form>
     </div>
--- test/unit/specs/stores/Setting.spec.js
+++ test/unit/specs/stores/Setting.spec.js
@@ -15,6 +15,11 @@ describe('setting', () => {
     expect(window.localStorage.getItem('nippouPath')).to.equal('/path/to/nippous')
   })
 
+  it('#sectionTitles=', () => {
+    setting.sectionTitles = '# section1'
+    expect(window.localStorage.getItem('sectionTitles')).to.equal('# section1')
+  })
+
   it('#authorizationKey', () => {
     window.localStorage.setItem('authorizationKey', 'xxx')
     expect(setting.authorizationKey).to.equal('xxx')
@@ -23,5 +28,15 @@ describe('setting', () => {
   it('#nippouPath', () => {
     window.localStorage.setItem('nippouPath', '/path')
     expect(setting.nippouPath).to.equal('/path')
+  })
+
+  it('#sectionTitles', () => {
+    window.localStorage.setItem('sectionTitles', '# section1\n# section2')
+    expect(setting.sectionTitles).to.equal('# section1\n# section2')
+  })
+
+  it('#listSectionTitles', () => {
+    window.localStorage.setItem('sectionTitles', '# section1\n# section2')
+    expect(setting.listSectinTitles).to.eql(['# section1', '# section2'])
   })
 })
