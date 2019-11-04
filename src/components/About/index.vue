cf0320d732b1e2f891677261be9d557f3cf64b83
Katsuya Hidaka <hidakatsuya@gmail.com>
2019-11-04 21:12:30 +0900

    Unify indentation

--- src/components/About/index.vue
+++ src/components/About/index.vue
@@ -1,19 +1,19 @@
-<style>
-  .about {
-    text-align: center;
-  }
-  .app-logo {
-    width: 80px;
-    height: 80px;
-  }
-  .app-name {
-    margin-top: 10px;
-  }
-  .app-version {
-    color: #aaa;
-    font-size: .9em;
-    margin-top: 10px;
-  }
+<style scoped>
+.about {
+  text-align: center;
+}
+.app-logo {
+  width: 80px;
+  height: 80px;
+}
+.app-name {
+  margin-top: 10px;
+}
+.app-version {
+  color: #aaa;
+  font-size: .9em;
+  margin-top: 10px;
+}
 </style>
 
 <template>
@@ -30,19 +30,19 @@
 </template>
 
 <script>
-  import Toolbar from './Toolbar'
+import Toolbar from './Toolbar'
 
-  export default {
-    components: {
-      Toolbar
+export default {
+  components: {
+    Toolbar
+  },
+  computed: {
+    version () {
+      return '0.1.0'
     },
-    computed: {
-      version () {
-        return '0.1.0'
-      },
-      productName () {
-        return 'Nippou Player'
-      }
+    productName () {
+      return 'Nippou Player'
     }
   }
+}
 </script>
