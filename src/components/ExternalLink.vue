8507175bde1673026d38404b990b4dc01757d9d8
Katsuya Hidaka <hidakatsuya@gmail.com>
2019-11-04 16:04:16 +0900

    Add a component to resolve external links universally with electron and browser

---
+++ src/components/ExternalLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <a :href="href" @click="onClick" target="_blank" class="external-link">
+    <slot></slot>
+  </a>
+</template>
+
+<script>
+export default {
+  props: ['href'],
+  methods: {
+    onClick (e) {
+      if (process.env.NODE_ENV === 'production') {
+        import('electron').shell.openExternal(this.href)
+        e.preventDefault()
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
