04aa2cfbb58db058412202a1faf1e6cb03102c69
Katsuya Hidaka <hidakatsuya@gmail.com>
2019-11-10 13:01:50 +0900

    Window settings

--- src/background.js
+++ src/background.js
@@ -17,9 +17,14 @@ protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600, webPreferences: {
-    nodeIntegration: true
-  }})
+  win = new BrowserWindow({
+    width: 500,
+    height: 280,
+    resizable: process.env.NODE_ENV === 'development',
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
