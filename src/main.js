c2e89e6925e540be747be549aebedaece2a5fad3
Katsuya Hidaka <hidakatsuya@gmail.com>
2019-11-11 00:48:36 +0900

    Setup PWA

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import routes from './routes'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
