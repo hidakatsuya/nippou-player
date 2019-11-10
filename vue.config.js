055ced3d173328cbe194082ce62fae85d4d9686c
Katsuya Hidaka <hidakatsuya@gmail.com>
2019-11-11 03:34:00 +0900

    Setup icons for PWA

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,14 @@ const path = require('path')
 module.exports = {
   pwa: {
     name: 'Nippou Player',
-    themeColor: '#0a9b94'
+    themeColor: '#0a9b94',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/mstile-150x150.png'
+    }
   },
   configureWebpack: {
     resolve: {
