64aaa1189673b086ea94f3f5b63a22e340f2bb60
Katsuya Hidaka <hidakatsuya@gmail.com>
2019-11-11 03:48:54 +0900

    Setup deployment to github pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/nippou-player/' : '',
   pwa: {
     name: 'Nippou Player',
     themeColor: '#0a9b94',
